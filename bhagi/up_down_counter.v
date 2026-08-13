42c1e43b2604bc1dda86f00ac6aa939258a610e1
bugidaybhagya-netizen <bugidaybhagya@gmail.com>
2026-08-13 12:21:52 +0530

    Add files via upload

---
+++ bhagi/up_down_counter.v
@@ -0,0 +1,17 @@
+module up_down_counter (
+    input clk,
+    input reset,
+    input up_down,
+    output reg [3:0] count
+);
+
+always @(posedge clk) begin
+    if (reset)
+        count <= 4'b0000;
+    else if (up_down)
+        count <= count + 1'b1;   // Up counting
+    else
+        count <= count - 1'b1;   // Down counting
+end
+
+endmodule
